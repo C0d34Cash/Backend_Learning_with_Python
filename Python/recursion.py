1038a714502d5f5114a3d03de4506c7cb1b877e9
C0d34Cash <mondaldebojyoti04@gmail.com>
2026-09-13 22:23:07 +0530

    Update recursions.py

--- Python/recursion.py
+++ Python/recursion.py
@@ -1,14 +1,13 @@
 # Function calls itself
 
 def add_one(num):
-    if (num >= 9):
-        return num + 1
-    total = num + 1
-    print(total)
+    if (num >= 9):      # if num is 9 or more, STOP and return
+        return num + 1  # return 10, but DON'T print it
+    total = num + 1     # 1,2,3...
+    print(total)        # prints here
+    return add_one(total) # call again
 
-    return add_one(total)
-
-add_one(0)   # Prints till 9 
+add_one(0) # Prints till 9
 
 print("\n\n\n\n")
 
@@ -20,5 +19,5 @@ def add_one(num):
 
     return add_one(total)
 
-mynewtotal = add_one(0)
-print(mynewtotal)      # Prints till 10
+mynewtotal = add_one(0) # add_one returns 10 at the end
+print(mynewtotal)       # so you print that 10
