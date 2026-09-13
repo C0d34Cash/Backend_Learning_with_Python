9b0d10f4ca64540ce53c72928278625c96b0fec2
C0d34Cash <mondaldebojyoti04@gmail.com>
2026-09-13 21:42:18 +0530

    Update functions.py

--- Python/functions.py
+++ Python/functions.py
@@ -5,7 +5,9 @@ def hello():
 hello()
 
 def sum(num1,num2):
-    print(num1+num2)
+    if (type(num1) is not int or type(num2) is not int):
+        return
+    return num1+num2
 
-sum(2,3)
-sum(-2,6)
+total = sum( )
+print(total)
