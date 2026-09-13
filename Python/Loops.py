5515ed31e9aaa2aeb0ef11b2fc1fec9b17df6a41
C0d34Cash <mondaldebojyoti04@gmail.com>
2026-09-13 20:01:28 +0530

    Update Loops.py

--- Python/Loops.py
+++ Python/Loops.py
@@ -1,37 +1,38 @@
-# value = 1
-# while value <= 10:
-#     print(value)
-#     if value == 5:
-#         break
-#     value += 1
-
-
 value = 1
-while value <= 10:
+while value <=10:
     value += 1
     if value == 5:
         continue
     print(value)
-else:
-    print("now value is equal to " + str(value))
+else: 
+    print("Value is now equal to " + str(value))
 
-names = ["Debojyoti","Ankan","Sandipan"]
-# for x in names:
-#     print(x)
-
-
-# for x in ["Mississippi"]:
-#     print(x)
-
-
-# for x in names:
-#     if x == "Sandipan":
-#         break
-#     print(x)
-
+names = ["Debojyoti", "Sandipan","Ankan" ]
+for x in names:
+    print(x)
 
 
 for x in names:
-    if x == "Sandipan":
-        continue
+    if x == "Ankan":
+        break
     print(x)
+
+for x in range(4):
+    print(x)
+
+for x in range(2,4):
+    print(x)
+
+
+print("\n\n")
+names = ["Debojyoti", "Sandipan","Ankan" ]
+actions = ["reads","eats","codes"]
+
+for name in names:
+    for action in actions:
+        print(name+ " " + action+ ".")
+
+print("\n\n\n")
+for action in actions:
+    for name in names:
+          print(name+ " " + action+ ".")
