fd573b9c1fcc60d8c6ee8a05f56dfac8946dd661
C0d34Cash <mondaldebojyoti04@gmail.com>
2026-09-13 22:19:51 +0530

    Update recursions.py

--- Python/recursion.py
+++ Python/recursion.py
@@ -8,5 +8,17 @@ def add_one(num):
 
     return add_one(total)
 
+add_one(0)   # Prints till 9 
+
+print("\n\n\n\n")
+
+def add_one(num):
+    if (num >= 9):
+        return num + 1
+    total = num + 1
+    print(total)
+
+    return add_one(total)
+
 mynewtotal = add_one(0)
-print(mynewtotal)
+print(mynewtotal)      # Prints till 10
