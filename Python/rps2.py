409cd2928e431c9c54c986b1d238fea9a0560f21
C0d34Cash <mondaldebojyoti04@gmail.com>
2026-09-13 21:05:14 +0530

    Update rps2.py

--- Python/rps2.py
+++ Python/rps2.py
@@ -7,31 +7,41 @@ class RPS(Enum):
     PAPER = 2
     SCISSORS = 3
 
+playagain = True
 
+while playagain:
+    playerchoice = input(
+        "\nEnter .... \n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
 
-print(' ')
-playerchoice = input("Enter .... \n1  for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+    player = int(playerchoice) # <-- This line was not indented
 
-player = int(playerchoice)
+    if player < 1 or player > 3:
+        sys.exit("Invalid choice. Please enter a number between 1 and 3.")
+        
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
 
-if player < 1 or player > 3:
-   sys.exit("Invalid choice. Please enter a number between 1 and 3.")
-    
-computerchoice = random.choice("123")
+    print(f"\nYou chose " + str(RPS(player)).replace("RPS."," ") + ".")
+    print(f"Computer chose " + str(RPS(computer)).replace("RPS."," ") +".\n")
 
-computer = int(computerchoice)
+    if player == 1 and computer == 3:
+        print("🎉 you win!")
+    elif player == 2 and computer == 1:
+        print("🎉 you win!")
+    elif player == 3 and computer == 2:
+        print("🎉 you win!")
+    elif player == computer:
+        print("😒 It's a tie!")
+    else:
+        print("🐍 Python wins!")
 
-print(" ")
-print(f"You chose " +  str(RPS(player)).replace("RPS."," ") + ".")
-print(f"Computer chose " + str(RPS(computer)).replace("RPS."," ") +".")
+    playagain = input("\n Play again? \nY for Yes or \nQ to Quit \n\n")
 
-if player == 1 and computer == 3:
-   print("you win")
-elif player == 2 and computer == 1:
-   print("you win")
-elif player == 3 and computer == 2:
-   print("you win")
-elif player == computer:
-   print("It's a tie")
-else:
-   print("Computer wins")
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("\n  🎉🎉🎉🎉🎉 ")
+        print("Thank you for playing!\n")
+        playagain = False
+
+sys.exit("Bye!")
